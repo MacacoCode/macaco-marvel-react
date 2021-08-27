47dbbe5432263e5f2dcf2aab7c6c602e94148558
Marco Narvaez <marcosnarvaez2010@gmail.com>
2021-08-26 19:45:18 -0600

    New dependency for prop types Improvement on spiderman animation to have loading spin if necessary

--- src/components/animations/SpidermanAnimation.js
+++ src/components/animations/SpidermanAnimation.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './SpidermanAnimation.css';
 
-const SpidermanAnimation = () => (
+const SpidermanAnimation = ({ loading }) => (
     <div id="spiderman-container">
-        <img className="spiderman" src="https://i.ibb.co/XDFkXMx/spiderman-colgado.png" alt="" />
+        <img className="spiderman" src="https://i.ibb.co/XDFkXMx/spiderman-colgado.png" alt="spiderman-ani" />
+        {loading}
         <div className="shadow"></div>
     </div>
 );
+SpidermanAnimation.defaultProps = {
+  loading: <></>,
+}
+
+SpidermanAnimation.propTypes = {
+  loading: PropTypes.node,
+}
 
 export default SpidermanAnimation;
 
--- src/pages/ComicsPage.js
+++ src/pages/ComicsPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Loading from '../components/animations/Loading';
+import SpidermanAnimation from '../components/animations/SpidermanAnimation';
 
 const ComicsPage = () => {
-    
+
     return (
         <>
+        <SpidermanAnimation loading={<Loading />} />
         </>
     );
 }
