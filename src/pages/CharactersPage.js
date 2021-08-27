1dc85bca5d7a2bab248d1626cf7287f8dce7fe92
Marco Narvaez <marcosnarvaez2010@gmail.com>
2021-08-27 08:50:40 -0600

    ENV To have hot refresh The fetch to get the first page I will be using

--- src/pages/CharactersPage.js
+++ src/pages/CharactersPage.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import SpidermanAnimation from '../components/animations/SpidermanAnimation';
+import { API_KEY, MARVEL_API } from '../constants';
+import useFetch from '../hooks/useFetch';
 
 const CharactersPage = () => {
+    const { response, loading, error, status } = useFetch(`${MARVEL_API}/v1/public/characters${API_KEY}&offset=0&limit=16`)
+    if (loading) return <SpidermanAnimation loading={true} />
+    console.log(response, error, status);
     return (
-    <>
-    <SpidermanAnimation loading={true} />
-    </>
+        <>
+        </>
     );
 }
 
