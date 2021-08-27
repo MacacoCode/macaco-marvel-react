778c5619be767daf45ec4379fb3196a0d54f8269
Marco Narvaez <marcosnarvaez2010@gmail.com>
2021-08-26 20:47:07 -0600

    Comics page test

--- src/pages/ComicsPage.js
+++ src/pages/ComicsPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Loading from '../components/animations/Loading';
 import SpidermanAnimation from '../components/animations/SpidermanAnimation';
+import { API_KEY, MARVEL_API } from '../constants';
+import useFetch from '../hooks/useFetch';
 
 const ComicsPage = () => {
-
+    const { data, loading, error, status } = useFetch(`${MARVEL_API}/v1/public/comics${API_KEY}&offset=0&limit=16`)
+    if (loading) return <SpidermanAnimation loading={true} />
+    console.log(data);
     return (
         <>
-        <SpidermanAnimation loading={true} />
         </>
     );
 }
