fdc80a9d61806d006cc2edd2d0bd01d76931a3ce
Marco Narvaez <marcosnarvaez2010@gmail.com>
2021-09-02 20:15:46 -0600

    Prop validation

--- src/components/templates/Searchbar.js
+++ src/components/templates/Searchbar.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Templates.css';
 
-const Searchbar = () => (
+const Searchbar = ({ onInputChange, onSearch }) => (
   <div className="searchbar-container">
     <div className="searchbar-input">
-      <input></input>
+      <input onChange={onInputChange}></input>
     </div>
     <div className="searchbar-search">
-      <button>GO!</button>
+      <button onClick={onSearch}>GO!</button>
     </div>
   </div>
 );
+
+Searchbar.defaultProps = {
+  onSearch: () => {},
+  onInputChange: () => {},
+};
+
+Searchbar.propTypes = {
+  onSearch: PropTypes.func,
+  onInputChange: PropTypes.func,
+};
 
 export default Searchbar;
